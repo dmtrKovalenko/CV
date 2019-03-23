3bfe6fe5a48c2ebc436a2d8cbcbc923e98de8709
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-03-23 19:12:24 +0200

    Refactor forHrs section

--- pages/forHrs.tsx
+++ pages/forHrs.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
-import { Page } from "../src/_shared/Page";
-import { Typography, Grid } from "@material-ui/core";
-import { ChipArray } from "../src/_shared/ChipArray";
-import { styled, makeStyles } from "@material-ui/styles";
+import Head from "next/head";
 import WebIcon from "@material-ui/icons/Web";
 import CodeIcon from "@material-ui/icons/Code";
 import ToolsIcon from "@material-ui/icons/Settings";
@@ -10,7 +7,13 @@ import CloudIcon from "@material-ui/icons/CloudQueue";
 import PhoneIcon from "@material-ui/icons/DeveloperMode";
 import TestingIcon from "@material-ui/icons/Cached";
 import skills from "../content/skills.json";
-import Head from "next/head";
+import { Page } from "../src/_shared/Page";
+import { Typography, Grid } from "@material-ui/core";
+import { ChipArray } from "../src/_shared/ChipArray";
+import { styled, makeStyles } from "@material-ui/styles";
+import { GridSize } from "@material-ui/core/Grid";
+import { SvgIconProps } from "@material-ui/core/SvgIcon";
+import { Breakpoint } from "@material-ui/core/styles/createBreakpoints";
 
 const GridItem = styled(Grid)({ marginBottom: 32 });
 
@@ -26,7 +29,27 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const forHrs: React.FunctionComponent<{}> = () => {
+type SkillSetProps = {
+  skill: keyof typeof skills;
+  Icon: React.ComponentType<SvgIconProps>;
+  GridProps?: Partial<Record<Breakpoint, boolean | GridSize>>
+}
+
+const SkillSet: React.FC<SkillSetProps> = ({ skill, Icon, GridProps }) => {
+  const styles = useStyles()
+  return ( 
+    <GridItem item xs={12} sm={4} {...GridProps}>
+      <Icon className={styles.icon} />
+      <Typography variant="h5" align="center" gutterBottom>
+        {skill}
+      </Typography>
+
+      <ChipArray items={skills[skill]} />
+    </GridItem>
+  )
+}
+
+const forHrs: React.FC = () => {
   const styles = useStyles();
 
   return (
@@ -45,59 +68,12 @@ const forHrs: React.FunctionComponent<{}> = () => {
       </Typography>
 
       <Grid container justify="center">
-        <GridItem item container xs={12} justify="center" direction="column">
-          <CodeIcon className={styles.icon} />
-          <Typography variant="h5" align="center" gutterBottom>
-            Languages
-          </Typography>
-
-          <ChipArray items={skills["Programming languages"]} />
-        </GridItem>
-
-        <GridItem item xs={12} sm={4}>
-          <WebIcon className={styles.icon} />
-          <Typography variant="h5" align="center" gutterBottom>
-            Front-End
-          </Typography>
-
-          <ChipArray items={skills["Front-end"]} />
-        </GridItem>
-
-        <GridItem item xs={12} sm={4}>
-          <CloudIcon className={styles.icon} />
-          <Typography variant="h5" align="center" gutterBottom>
-            Back-End
-          </Typography>
-
-          <ChipArray items={skills["Back-end"]} />
-        </GridItem>
-
-        <GridItem item xs={12} sm={4}>
-          <PhoneIcon className={styles.icon} />
-          <Typography variant="h5" align="center" gutterBottom>
-            Mobile
-          </Typography>
-
-          <ChipArray items={skills["Mobile"]} />
-        </GridItem>
-
-        <GridItem item xs={12} sm={4}>
-          <TestingIcon className={styles.icon} />
-          <Typography variant="h5" align="center" gutterBottom>
-            Testing
-          </Typography>
-
-          <ChipArray items={skills["Testing"]} />
-        </GridItem>
-
-        <GridItem item xs={12} sm={4}>
-          <ToolsIcon className={styles.icon} />
-          <Typography variant="h5" align="center" gutterBottom>
-            Tools
-          </Typography>
-
-          <ChipArray items={skills['Tools']} />
-        </GridItem>
+        <SkillSet skill="Languages" Icon={CodeIcon} GridProps={{ sm: 12 }} />
+        <SkillSet skill="Front-end" Icon={WebIcon} />
+        <SkillSet skill="Back-end" Icon={CloudIcon} />
+        <SkillSet skill="Mobile" Icon={PhoneIcon} />
+        <SkillSet skill="Testing" Icon={TestingIcon} />
+        <SkillSet skill="Tools" Icon={ToolsIcon} />
       </Grid>
     </Page>
   );
