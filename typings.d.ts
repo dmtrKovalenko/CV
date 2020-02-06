c5c5186afe33f2e03631acd60ccb18a698b5f1ff
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-02-06 19:46:02 +0200

    Fix typescript errors

---
+++ typings.d.ts
@@ -0,0 +1,20 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.md" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "typeit" {
+  const value: any;
+  export default value;
+}
+
