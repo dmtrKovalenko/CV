0b107871a95213889c868a3e7292481453b9af73
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-12-11 12:13:23 +0200

    Add cypress tests

---
+++ cypress/integration/visual-regression.spec.ts
@@ -0,0 +1,21 @@
+describe("dmtrkovalenko.dev", () => {
+  it("opens landing page", () => {
+    cy.visit("/")
+    cy.percySnapshot()
+  })
+
+  it("opens video talk card", () => {
+    cy.visit("/")
+    cy.contains("Stop using console for component testing").click()
+    cy.percySnapshot()
+  })
+
+  it("opens for-hrs page", () => {
+    cy.visit("/forHrs")
+  })
+
+  it("opens resume html page", () => {
+    cy.visit("/resume")
+    cy.percySnapshot()
+  })
+})
